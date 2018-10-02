b22d91583c04a8c6e69ab787a3d503abc65409b9
Katrina <pritckat@gmail.com>
2018-10-02 15:54:49 -0500

    create routes for logging in

--- overwatchapp/app/controllers/sessions_controller.rb
+++ overwatchapp/app/controllers/sessions_controller.rb
@@ -1,8 +1,11 @@
 class SessionsController < ApplicationController
 
     def new
+        @user = User.new
     end
 
     def create
+        @user = User.find(params[:username])
+        redirect_to user_path(@user)
     end
 end
