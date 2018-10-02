6074aa0c605b55f9c187d0e519500e2e43007f6a
Katrina <pritckat@gmail.com>
2018-10-02 15:07:40 -0500

    add CR to userscontroller

--- overwatchapp/app/controllers/users_controller.rb
+++ overwatchapp/app/controllers/users_controller.rb
@@ -1,2 +1,21 @@
 class UsersController < ApplicationController
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.create(params[user_params])
+        redirect_to user_path(@user)
+    end
+
+    def show
+        @user = User.find_by(params[:id])
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username)
+    end
 end
